5b4207e262315d1991f6a8a40ba01c73dbb151ff
Sergio Villalta <6gal6ler6@gmail.com>
2024-06-05 19:57:18 -0600

    Updated

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Game from './components/Game';
 import OpponentSelection from './components/OpponentSelection';
 import { GameProvider } from './context/GameContext';
